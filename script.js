8a8cc2923449c2dbbf2ac613762f19d09bd76c41
Jason <kangedklan@gmail.com>
2019-08-08 16:29:57 -0400

    can read user input now

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var keyWord = prompt("Enter a keyword to search for:");
+keyWord = String(keyWord).toLowerCase();
+
 var swiper = setInterval(function() {
 	//used Chrome DevTools to find class names for profile button
 	var openProfile = document.getElementsByClassName("Pos(a) End(16px) B(40px) Trsdu($normal) Sq(40px) Lh(40px) Bdrs(50%) Cur(p) Ta(c) Fl(end) Scale(1.2):h")[0];
@@ -8,8 +11,8 @@ var swiper = setInterval(function() {
 		var profileText = profileBio != null ? profileBio.innerText : "";
 		console.log("Reading bio... \n"+profileText);
 
-		profileText.toLowerCase();
-		if(profileText.indexOf("travel") != -1) {
+		profileText = profileText.toLowerCase();
+		if(profileText.indexOf(keyWord) != -1) {
 			console.log("Match found! Liking...");
 			//clicking like button
 			document.getElementsByClassName("button Lts($ls-s) Z(0) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) Wc($transform) End(15px) Scale(1.1):h")[0].click();
